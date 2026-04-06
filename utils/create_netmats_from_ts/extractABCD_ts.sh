536a66bb0e250b235dd9a09e3ece1051dcb4636b
naranjorincon <naranjorincon@wust.edu>
2026-04-06 14:04:33 -0500

    updating so that we can use NEW ABCD data from newest 6.0 release. Data is curated, for many sessions, and will have access to brain representations of: Gradients, ICA, Profumo, MS-HBM, and other individualized topography mappinds. Additionally, added netmat scripts so that all subjects also have corresponding schaefer 100/300 and glasser 360 netmat types for future expansions od translation capabilities.

--- utils/create_netmats_from_ts/extractABCD_ts.sh
+++ utils/create_netmats_from_ts/extractABCD_ts.sh
@@ -1,49 +1,46 @@
 #!/bin/bash
-#SBATCH -J "ExtractABCD_Timeseries"
+#SBATCH -J ExtractABCD_Timeseries
 #SBATCH -o /ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/surf2netmat/batch/ExtractABCD_Timeseries_Schaefer200.out%j
 #SBATCH -e /ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/surf2netmat/batch/ExtractABCD_Timeseries_Schaefer200.err%j
 #SBATCH --partition=tier2_cpu 
 #SBATCH --account=janine_bijsterbosch
-#SBATCH --mem-per-cpu 4G 
-#SBATCH --cpus-per-task 6
 #SBATCH -t 0-12:00:00 
 
 module load workbench
 
 # need subj ID file
-file="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/generate_ICA/ABCD_ICA_subjids.txt"
+subjID_fpath="/ceph/chpc/shared/janine_bijsterbosch_group/WAPIAW_2026/qc/5min_pconn_subjects.txt"
+# subjID_fpath="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/wapiaw26/subject_list_only5.txt"
 
 # Schaefer100
-parcel_file="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/schaefer100/Schaefer2018_100Parcels_17Networks_order.dlabel.nii"
-dir_path="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/schaefer100/timeseries"
-
-# Schaefer200
-#parcel_file="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/schaefer200/Schaefer2018_200Parcels_17Networks_order.dlabel.nii"
-#dir_path="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/schaefer200/timeseries"
+# parcel_file="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/schaefer100/Schaefer2018_100Parcels_17Networks_order.dlabel.nii"
+# dir_path="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/new_ABCD_version/schaefer_d100/untranspose"
 
 # Schaefer300
-#parcel_file="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/schaefer300/Schaefer2018_300Parcels_17Networks_order.dlabel.nii"
-#dir_path="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/schaefer300/timeseries"
+# parcel_file="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/schaefer200/Schaefer2018_200Parcels_17Networks_order.dlabel.nii"
+# dir_path="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/new_ABCD_version/schaefer_d300/untranspose"
 
 # Glasser360
-#parcel_file="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/glasser360/Q1-Q6_RelatedParcellation210.CorticalAreas_dil_Colors_210P_Orig.32k_fs_LR.dlabel.nii"
-#dir_path="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/glasser360/timeseries"
+parcel_file="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/glasser360/Q1-Q6_RelatedParcellation210.CorticalAreas_dil_Colors_210P_Orig.32k_fs_LR.dlabel.nii"
+dir_path="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/new_ABCD_version/glasser_d360/untranspose"
 
-while IFS= read -r subject_id; 
+# loop
+mkdir -p ${dir_path}
+subjID_list=$(cat ${subjID_fpath})
+for subjID in ${subjID_list};
 do
-    subject_id=$(echo "$subject_id" | tr -d '\r')
-    echo "Extracting Timeseries For: $subject_id"
+    echo "Extracting Timeseries For: ${subjID}"
 
     wb_command -cifti-parcellate \
-        /ceph/chpc/rcif_datasets/abcd/abcd_collection3165/derivatives/abcd-hcp-pipeline/sub-${subject_id}/ses-baselineYear1Arm1/files/MNINonLinear/Results/task-rest_DCANBOLDProc_v4.0.0_Atlas.dtseries.nii \
+        /ceph/chpc/shared/janine_bijsterbosch_group/WAPIAW_2026/cortex_only_data/ses-all/4mm_smooth/${subjID}_cortex_only_demean_smooth_4mm.dtseries.nii \
         ${parcel_file} \
         COLUMN \
-        ${dir_path}/${subject_id}.ptseries.nii
+        ${dir_path}/${subjID}.ptseries.nii
 
     wb_command -cifti-convert -to-text \
-        ${dir_path}/${subject_id}.ptseries.nii \
-        ${dir_path}/untranspose_${subject_id}.txt
+        ${dir_path}/${subjID}.ptseries.nii \
+        ${dir_path}/untranspose_${subjID}.txt
 
-done < "${file}"
+done #< "${subjID_fpath}"
 
 chmod 771 ${dir_path}/*
--- utils/create_netmats_from_ts/transpose_ptseries.sh
+++ utils/create_netmats_from_ts/transpose_ptseries.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-#SBATCH -J "TransposeABCD_tseries"
-#SBATCH -o /ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/logs/TransposeABCD_tseries.out%j
-#SBATCH -e /ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/logs/TransposeABCD_tseries.err%j
+#SBATCH -J TransposeABCD_tseries
+#SBATCH -o /ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/surf2netmat/batch/TransposeABCD_tseries.out%j
+#SBATCH -e /ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/surf2netmat/batch/TransposeABCD_tseries.err%j
 #SBATCH --partition=tier2_cpu 
 #SBATCH --account=janine_bijsterbosch
-#SBATCH --mem-per-cpu 4G 
-#SBATCH --cpus-per-task 10
 #SBATCH -t 0-23:00:00 
 
 source activate neurotranslate
 
 script_path="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/utils/transpose_ptseries.py"
-subjects_list_path="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/generate_ICA/ABCD_ICA_subjids.txt"
-untranspose_ts_path="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/schaefer100/timeseries/"
-save_path="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/schaefer100/timeseries/transposed_ts/"
+# subjects_list_path="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/wapiaw26/subject_list_only5.txt"
+subjects_list_path="/ceph/chpc/shared/janine_bijsterbosch_group/WAPIAW_2026/qc/5min_pconn_subjects.txt"
+untranspose_ts_path="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/new_ABCD_version/schaefer_d100/untranspose"
+save_path="/ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/new_ABCD_version/schaefer_d100/transpose"
+mkdir -p ${save_path}
 
 python ${script_path} ${subjects_list_path} ${untranspose_ts_path} ${save_path}
 
-chmod -R 771 /ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/
+chmod -R 771 /ceph/chpc/shared/janine_bijsterbosch_group/naranjorincon_scratch/NeuroTranslate/ABCD_NetMats/new_ABCD_version
